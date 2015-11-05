d52d417609708525c16610a1705c3b827b689908
Clazzasaur <clazzasaur@users.noreply.github.com>
2015-11-06 00:43:48 +1300

    HI

--- HelloSean.py
+++ HelloSean.py
@@ -3,7 +3,13 @@ __author__ = 'clazz'
 
 print "Hello Sean"
 print "Hello Cliff."
-print "I wonder what will happen?"print "Hello Cliff."
+print "I wonder what will happen?"
+print "Hello Cliff."
 print "Melons."
 print "Melons."
 print "I wonder what will happen?"
+
+
+
+
+print "I thought removing the shell from my snail would make it faster.... But it just made it more sluggish."
